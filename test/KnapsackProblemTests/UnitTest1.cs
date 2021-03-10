9dff37b432b8ee792c735bc5c4a6e49760a34daa
John15321 <janbronicki@gmail.com>
2021-03-10 12:03:33 +0100

    added tests in nunit

---
+++ test/KnapsackProblemTests/UnitTest1.cs
@@ -0,0 +1,18 @@
+using NUnit.Framework;
+
+namespace KnapsackProblemTests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
