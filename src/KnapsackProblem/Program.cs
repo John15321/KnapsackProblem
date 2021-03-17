dc376ac31ffae8eb8245e4780ba1258e2538b2cf
John15321 <janbronicki@gmail.com>
2021-03-17 14:30:22 +0100

    added tests

--- src/KnapsackProblem/Program.cs
+++ src/KnapsackProblem/Program.cs
@@ -11,32 +11,29 @@ namespace KnapsackProblem
     /// </summary>
     public class Item : IComparable<Item>
     {
-        public Item(int weight, int value)
-        {
-            Weight = weight;
-            Value = value;
-            ValueDensity = (double) value / weight;
-        }
-
         // Value of the item
         public int Value { get; }
-
         // Weight of the item
         public int Weight { get; }
-
         // Value x Weight density
         public bool InTheBag { get; set; }
-
         // If the item is going to be placed
         // in the bag, the value will be true
         // otherwise its going to be false
         public double ValueDensity { get; }
-
+        public Item(int weight, int value)
+        {
+            Weight = weight;
+            Value = value;
+            ValueDensity = (double)value / weight;
+        }
         public int CompareTo(Item other)
         {
             return ValueDensity.CompareTo(other.ValueDensity);
         }
     }
+
+
 
     public class SortByValueDensityDescending : IComparer<Item>
     {
@@ -49,6 +46,12 @@ namespace KnapsackProblem
     }
 
 
+
+
+
+
+
+
     /// <summary>
     /// The Knapsack class contains all the Items, and
     /// Items that will be stored in the bag
@@ -58,10 +61,9 @@ namespace KnapsackProblem
         private int _cSize;
         private readonly int _numberOfItems;
         private Item[] _items;
-
         // The seed for randomness
         private readonly long _seed;
-
+        private int valueSum;
 
         public Knapsack(int numberOfItems, int cSize, long seed)
         {
@@ -69,15 +71,14 @@ namespace KnapsackProblem
             _seed = seed;
             _cSize = cSize;
         }
-
-
         public int C
         {
             get => _cSize;
             set => _cSize = value;
         }
+        public int ValueSum { get => valueSum; set => valueSum = value; }
 
-        internal void GenerateItems()
+        public void GenerateItems()
         {
             RandomNumberGenerator rng = new RandomNumberGenerator(_seed);
             _items = new Item[_numberOfItems];
@@ -86,8 +87,7 @@ namespace KnapsackProblem
                 _items[i] = new Item(rng.nextInt(1, 29), rng.nextInt(1, 29));
             }
         }
-
-        internal void ShowItems()
+        public void ShowItems()
         {
             for (var i = 0; i < _items.Length; i++)
             {
@@ -95,25 +95,26 @@ namespace KnapsackProblem
                     $"Item {i + 1:00}| Density: {_items[i].ValueDensity:00.00}| Weight: {_items[i].Weight:00}| Value: {_items[i].Value:00}| InTheBag: {_items[i].InTheBag}");
             }
         }
-
-        internal void SortTheItems()
+        public void SortTheItems()
         {
             SortByValueDensityDescending sortByValueDensityDescending = new SortByValueDensityDescending();
             Array.Sort(_items, sortByValueDensityDescending);
         }
-
-        internal void FitTheItemsInTheBag()
+        public void FitTheItemsInTheBag()
         {
             var sum = 0;
             var i = 0;
-            while (sum < _cSize)
+            while (i < _numberOfItems)
             {
                 _items[i].InTheBag = true;
                 sum += _items[i].Weight;
+                if((sum + _items[i].Weight) >= _cSize)
+                {
+                    break;
+                }
                 i++;
             }
         }
-
         public void PrintTheBag()
         {
             var value_sum = 0;
@@ -124,10 +125,17 @@ namespace KnapsackProblem
                     value_sum += item.Value;
                 }
             }
-
+            valueSum = value_sum;
+            Console.WriteLine("---------------------------------------------------------------");
             Console.WriteLine($"Total value in the Knapsack: {value_sum:000}");
+            Console.WriteLine("---------------------------------------------------------------");
         }
     }
+
+
+
+
+
 
     class Program
     {
@@ -138,18 +146,23 @@ namespace KnapsackProblem
             var seed = 2137;
 
             var ourBag = new Knapsack(bagSize, C, seed);
+            Console.WriteLine("---------------------------------------------------------------");
+            Console.WriteLine("Randomized items: ");
+            Console.WriteLine("---------------------------------------------------------------");
             // Generate and initialize random items
             ourBag.GenerateItems();
             ourBag.ShowItems();
             // Sort the items by value x weight
             ourBag.SortTheItems();
+            Console.WriteLine("---------------------------------------------------------------");
             Console.WriteLine("Sorted");
+            Console.WriteLine("---------------------------------------------------------------");
             ourBag.ShowItems();
-            Console.WriteLine("---------------------------");
+            Console.WriteLine("---------------------------------------------------------------");
             // Fit the items in the bag
             ourBag.FitTheItemsInTheBag();
-            Console.WriteLine("---------------------------");
             Console.WriteLine("In the bag");
+            Console.WriteLine("---------------------------------------------------------------");
             ourBag.ShowItems();
             // Return the list of things
             ourBag.PrintTheBag();
--- test/KnapsackProblemTests/UnitTest1.cs
+++ test/KnapsackProblemTests/UnitTest1.cs
@@ -1,18 +1,82 @@
 using NUnit.Framework;
+using GeneratorCS;
+using KnapsackProblem;
 
 namespace KnapsackProblemTests
 {
     public class Tests
     {
+        private Item t1;
+        private Item[] lt = { new Item(1, 10), new Item(10, 1) };
+
         [SetUp]
         public void Setup()
         {
+            t1 = new Item(10, 10);
         }
 
         [Test]
-        public void Test1()
+        public void Test_ValueDensity_Calculation()
         {
-            Assert.Pass();
+            Assert.AreEqual(t1.ValueDensity, 1);
+        }
+
+        [Test]
+        public void Test_RandomNumberGenerator1()
+        {
+            RandomNumberGenerator rng = new RandomNumberGenerator(1);
+            int x = rng.nextInt(1, 29);
+            Assert.IsTrue(x >= 1);
+        }
+
+        [Test]
+        public void Test_RandomNumberGenerator2()
+        {
+            RandomNumberGenerator rng = new RandomNumberGenerator(1);
+            int x = rng.nextInt(1, 29);
+            Assert.IsTrue(x <= 29);
+        }
+
+        [Test]
+        public void Test_ValueSum1()
+        {
+            var bagSize = 30;
+            var C = 20;
+            var seed = 2137;
+            var testBag = new Knapsack(bagSize, C, seed);
+            testBag.GenerateItems();
+            testBag.SortTheItems();
+            testBag.FitTheItemsInTheBag();
+            testBag.PrintTheBag();
+            Assert.AreEqual(testBag.ValueSum, 61);
+        }
+
+        [Test]
+        public void Test_ValueSum2()
+        {
+            var bagSize = 10;
+            var C = 15;
+            var seed = 420;
+            var testBag = new Knapsack(bagSize, C, seed);
+            testBag.GenerateItems();
+            testBag.SortTheItems();
+            testBag.FitTheItemsInTheBag();
+            testBag.PrintTheBag();
+            Assert.AreEqual(testBag.ValueSum, 41);
+        }
+
+        [Test]
+        public void Test_ValueSum3()
+        {
+            var bagSize = 5;
+            var C = 10;
+            var seed = 609;
+            var testBag = new Knapsack(bagSize, C, seed);
+            testBag.GenerateItems();
+            testBag.SortTheItems();
+            testBag.FitTheItemsInTheBag();
+            testBag.PrintTheBag();
+            Assert.AreEqual(testBag.ValueSum, 14);
         }
     }
 }
